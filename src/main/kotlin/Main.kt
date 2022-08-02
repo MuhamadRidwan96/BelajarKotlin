f6c43e1b0bf2f97749574dcedc2988c2b399b9d3
Rdn-M <muhammadreadone96@gmail.com>
2022-08-02 20:58:03 +0700

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+const val APP = "Learning Kotlin"
+const val VERSION = "0.0.1"
+
+fun main() {
+ /* val firsName: String?
+      firsName = "eko"
+
+    println(firsName)
+    println(firsName.length)*/
+
+   /* println("$APP : $VERSION")*/
+
+
+}
+
---
+++ src/main/kotlin/Range.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val range = 1..100
+    println(range)
+    /*
+    *Range operation
+    * count == Get totals range data
+    * contains(value) == Check had a values
+    * first == get first value
+    * last == get last values
+    * step == get a value every increased*/
+}
