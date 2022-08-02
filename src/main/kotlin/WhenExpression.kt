2f0c263dfa8befd4bef76be5d5859109f05aca90
Rdn-M <muhammadreadone96@gmail.com>
2022-08-02 23:01:30 +0700

    initial commit

--- src/main/kotlin/WhenExpression.kt
+++ src/main/kotlin/WhenExpression.kt
@@ -1,2 +1,21 @@
-class WhenExpression {
+fun main() {
+    val finalExam = "D"
+
+    when(finalExam) {
+        "A" -> {
+            println("Awesome")
+        }
+        "B" -> {
+            println("Cool")
+        }
+        "C" -> {
+            println("Not Bad")
+        }
+        "D" -> {
+            println("Bad")
+        }
+        else ->{
+            println("Ups try next year")
+        }
+    }
 }
