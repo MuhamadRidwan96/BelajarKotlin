e923cac1bd475d4b462de8ad009d3302830574de
Rdn-M <muhammadreadone96@gmail.com>
2022-08-02 21:18:14 +0700

    initial commit

--- src/main/kotlin/Range.kt
+++ src/main/kotlin/Range.kt
@@ -1,6 +1,13 @@
 fun main() {
-    val range = 1..100
-    println(range)
+    //val range = 1..100
+    val range = 100 downTo 1 step 5
+
+    println(range.count())
+    println(range.contains(30))
+    println(range.contains(200))
+    println(range.first)
+    println(range.last)
+    println(range.step)
     /*
     *Range operation
     * count == Get totals range data
@@ -8,4 +15,6 @@ fun main() {
     * first == get first value
     * last == get last values
     * step == get a value every increased*/
+
+
 }
